62e959cd77d009d623ee225287cf2e143eeaee3d
crackcodesprogramloads <mikepadial@gmail.com>
2023-11-23 18:14:44 +0800

    Reduce interface shadow

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({
 }) {
   return (
     <button
-      className={`${width} py-4 px-8 text-xl text-zinc-200 hover:text-zinc-50 font-semibold border-l border-t border-gray-600 hover:border-gray-400 rounded-lg shadow-[0px_0px_60px] shadow-sky-700/70`}
+      className={`${width} py-4 px-8 text-xl text-zinc-200 hover:text-zinc-50 font-semibold border-l border-t border-gray-600 hover:border-gray-400 rounded-lg shadow-[0px_0px_50px] shadow-sky-700/70`}
     >
       {text}
     </button>
