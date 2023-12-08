5cb7729b9082ba70785a8b8cb75e58668bc1124b
crackcodesprogramloads <mikepadial@gmail.com>
2023-12-08 12:07:42 +0800

    Add heading and add submit button gradient

---
+++ app/components/Header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { m, LazyMotion, domAnimation } from "framer-motion";
+
+export default function Header() {
+  return (
+    <LazyMotion features={domAnimation}>
+      <m.h1
+        className="text-2xl text-zinc-200 uppercase"
+        initial={{ y: "-200%", opacity: 0.1 }}
+        animate={{ y: "0", opacity: 1 }}
+        transition={{
+          type: "spring",
+          damping: 20,
+          delay: 0,
+        }}
+      >
+        Move assets gas free and cross-chain
+      </m.h1>
+    </LazyMotion>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import UserInterface from "./components/UserInterface/UserInterface";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center">
+    <main className="w-full h-screen flex flex-col items-center justify-center gap-4">
+      <Header />
       <UserInterface />
     </main>
   );
